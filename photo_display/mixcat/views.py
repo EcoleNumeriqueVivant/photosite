4f4672119d34fe72b164ba50dc52f68fc9615b13
etienne ziegelmeyer <ziegelmeyer.etienne@gmail.com>
2018-03-15 20:42:23 +0100

    Add static_root on  settings

--- photo_display/mixcat/views.py
+++ photo_display/mixcat/views.py
@@ -1,17 +1,15 @@
 import os
 import random
 from django.shortcuts import render
-from django import static
-from django.conf import settings
+from mixcat import static
+# from django import static
+# from django.conf import settings
 
 # from .models import Picture
-register = 
+
 
 def index(request):
     return render(request, 'mixcat/index.html', {})
 
 def catpict(request):
     return render(request, 'mixcat/catimg.html', {})
-
-def random_image(request):
-    pick = random.choice()
--- photo_display/photo_display/settings.py
+++ photo_display/photo_display/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mixcat.apps.MixcatConfig',
-    'random_image'
+    # 'random_image'
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
