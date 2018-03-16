964494a2715a03e2ff27f02b25dd59274a303f9d
Clement Hussenot <chussenota@gmail.com>
2018-03-16 11:56:30 +0100

    WIP (Exercice 2) - Store images in a db ...

--- photositeBene/photo/views.py
+++ photositeBene/photo/views.py
@@ -5,13 +5,18 @@ import os
 import random
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_IMG = os.path.join(BASE_DIR, 'photo/static/pictures')
+STATIC_PHOTO = os.path.join(BASE_DIR, 'photo')
+STATIC_IMG = os.path.join(STATIC_PHOTO, 'static/pictures')
 
 def photo(request):
+    db = open(STATIC_PHOTO + '/db.csv', 'r+')
     images = os.listdir(STATIC_IMG)
+    already_displayed_images = db.readlines()
+    images = list(set(images) - set(already_displayed_images)) 
     if images:
         random.shuffle(images)
         image = images[0]
+        db.write(image + "\n")
         return HttpResponse("<img src=\"/static/pictures/"+ image + "\" alt=\"A random image\"/>")
     else:
         return HttpResponse("no images")
