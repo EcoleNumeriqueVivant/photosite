4b58dc9d971136172fede1981f582a65ef29a386
etienne ziegelmeyer <ziegelmeyer.etienne@gmail.com>
2018-03-21 23:42:13 +0100

    Add picture link & templates

--- photo_display/mixcat/views.py
+++ photo_display/mixcat/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+import os
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATIC_IMG = os.path.join(BASE_DIR, "mixcat/static/img")
-# images = os.listdir(STATIC_IMG)
-# from .models import Picture
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_IMG = os.path.join(BASE_DIR, "mixcat/static/mixcat/img")
+
 
 
 def catpict(request):
-    images = []
-    return render(request, 'mixcat/mixcat.html', {"images": images})
+    images = os.listdir(STATIC_IMG)
+    image = images[0]
+    return render(request, 'mixcat/mixcat.html', {"images": image})
