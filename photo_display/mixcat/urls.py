d18b5ca78f14e33083212dac095c1ef4edfae62d
etienne ziegelmeyer <ziegelmeyer.etienne@gmail.com>
2018-03-28 15:35:22 +0200

    Resolve probleme afficher image chat

--- photo_display/mixcat/urls.py
+++ photo_display/mixcat/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
+from mixcat.views import catpict
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views. catpict),
+    url(r'^$', views.catpict),
 
 
 ]
--- photo_display/mixcat/views.py
+++ photo_display/mixcat/views.py
@@ -9,5 +9,4 @@ STATIC_IMG = os.path.join(BASE_DIR, "mixcat/static/mixcat/img")
 def catpict(request):
     images = os.listdir(STATIC_IMG)
     image = images[0]
-    print('jkhids')
-    return render(request, 'mixcat/mixcat.html', {'catpict': image})
+    return render(request, 'mixcat/mixcat.html', {'imagechat': image})
